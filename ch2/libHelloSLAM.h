9bea114d09be494366ab278c444bcdc842680a39
Ayabaabaaba <2647548097@qq.com>
2024-09-01 18:39:31 +0800

    20240901_ch2更新

---
+++ ch2/libHelloSLAM.h
@@ -0,0 +1,8 @@
+#ifndef LIBHELLOSLAM_H_
+#define LIBHELLOSLAM_H_
+//上面宏定义，防止重复引用头文件引起的重定义错误
+
+//函数声明
+void printHello();
+
+#endif
---
+++ ch2/useHello.cpp
@@ -0,0 +1,7 @@
+#include "libHelloSLAM.h"
+
+//使用库里的函数
+int main(int argc, char **argv){
+printHello();
+return 0;
+}
